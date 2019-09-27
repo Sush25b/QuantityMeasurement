845a0ba4ed805cbe385d23ed5fafec592c4c3791
Sushant Sukhadeo Phulsundar <sushantsukhadeophulsundar@Sushants-MacBook-Pro.local>
2019-09-27 10:08:24 +0530

    [Sushant]

---
+++ src/main/java/com/thoughtworks/measure/QunatityMeasurement.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.measure;
+
+public class QunatityMeasurement
+{
+    private int length;
+
+    public QunatityMeasurement() {
+    }
+
+    public int compare(int i) {
+        return 0;
+    }
+}
--- src/test/java/com/thoughtworks/measure/QuantityMeasurementTest.java
+++ src/test/java/com/thoughtworks/measure/QuantityMeasurementTest.java
@@ -3,13 +3,13 @@ package com.thoughtworks.measure;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class LengthMeasurementTest
+public class QuantityMeasurementTest
 {
     @Test
     public void givenFeetAsZero_whenCompareLength_thenReturnZeroInch()
     {
-        LengthMeasurement lengthMeasurement = new LengthMeasurement();
-        Assert.assertEquals(0,lengthMeasurement.measure(0));
+        QunatityMeasurement lengthMeasurement = new QunatityMeasurement();
+        Assert.assertEquals(0,lengthMeasurement.compare(0));
     }
 
 }
